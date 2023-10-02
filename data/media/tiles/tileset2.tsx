6c77a1c11e4e26a196441bb7d3988c01ed5e97c8
Rijad Ismailović <94862276+Rijad-Ismailovic@users.noreply.github.com>
2023-10-02 15:42:02 +0200

    Add files via upload

---
+++ data/media/tiles/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset2" tilewidth="16" tileheight="16" spacing="1" tilecount="18" columns="3">
+ <image source="tileset2.png" trans="ffffff" width="50" height="101"/>
+</tileset>
---
+++ data/media/tiles/tileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset3" tilewidth="16" tileheight="16" spacing="1" tilecount="30" columns="3">
+ <image source="tileset3.png" trans="ffffff" width="50" height="169"/>
+</tileset>
